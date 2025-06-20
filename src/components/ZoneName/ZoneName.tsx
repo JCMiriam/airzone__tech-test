03e61191aa8cac246b6d496f589108eedb4b2089
JCMiriam <myriamkey612@gmail.com>
2025-06-21 01:47:36 +0200

    feat(zone-name): update and style zone name component

--- src/components/ZoneName/ZoneName.tsx
+++ src/components/ZoneName/ZoneName.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { ZoneNameProps } from './ZoneName.types';
 
-export const ZoneName: React.FC<ZoneNameProps> = ({ name }) => {
+import styles from './ZoneName.module.scss';
+
+export const ZoneName: React.FC<ZoneNameProps> = ({ name, isOn }) => {
+    const className = isOn
+        ? styles.zoneNameOn
+        : styles.zoneNameOff;
+
     return (
-        <h2 data-testid="zone-name">
+        <h2 data-testid="zone-name" className={`${styles.zoneName} ${className}`}>
             {name}
         </h2>
     );
--- src/components/ZoneName/ZoneName.types.ts
+++ src/components/ZoneName/ZoneName.types.ts
@@ -1,4 +1,5 @@
 export type ZoneNameProps = {
     name: string;
     className?: string;
+    isOn?: boolean;
 };
