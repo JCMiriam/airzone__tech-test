a9ffbd9be2f3839fa4e7940a3a9cc4dccaca2713
JCMiriam <myriamkey612@gmail.com>
2025-06-21 12:34:54 +0200

    feat(layout): create layout component to envolve any page with prupose of unify common styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Layout from "./ui/components/LayoutComponent/Layout";
 import ZonesPage from "./pages/ZonesPage/ZonesPage";
 
 function App() {
-  return <ZonesPage />;
+  return (
+    <Layout>
+      <ZonesPage />
+    </Layout>
+  );
 }
 
 export default App;
--- src/pages/ZonesPage/ZonesPage.tsx
+++ src/pages/ZonesPage/ZonesPage.tsx
@@ -4,9 +4,9 @@ import styles from "./ZonesPage.module.scss";
 const ZonesPage: React.FC = () => {
   return (
     <main className={styles.zonesPage}>
-      <div>
-        <h2>My zones</h2>
-        <div>
+      <div className={styles.zonesTitle}>
+        <h2 className={styles.zonesTitle__text}>My zones</h2>
+        <div className={styles.zonesTitle__settings}>
           <span></span>
           <span></span>
           <span></span>
